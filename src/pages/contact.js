2c00f85ac572697e85a2a121ceb16b8be7a9c54f
samuelaparker <samuel.a.parker00@gmail.com>
2021-07-12 11:13:02 -0400

    build

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,16 @@
 import React from "react"
 import Contact from "../components/form"
 import Layout from '../components/layout';
-import styled from "styled-components";
-
-const ContactWrapper = styled.div`
-  /* margin-top: 30px;
-  width: 30vw; */
-`;
 
 
 
 const ContactPage = () => {
     return (
         <Layout pageTitle="Contact">
-            <ContactWrapper>
-            <Contact /> 
-            </ContactWrapper>
-         
+            <div>
+                <Contact/>
+            </div>
+           
         </Layout>
         
     )
