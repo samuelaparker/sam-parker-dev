38892b7cdaa29d5aa36100ec65f8361fcea1867a
samuelaparker <samuel.a.parker00@gmail.com>
2021-07-28 12:56:09 -0400

    build

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 export default function Card({ firstName, lastName, thumbnail, location }) {
 
-    
     return (
         <StyledItem>
             <StyledThumbnailWrapper>
