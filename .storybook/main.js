f9d6719d1b0ed0362812669977a256ddb939ffc6
samuelaparker <samuel.a.parker00@gmail.com>
2021-08-03 21:59:38 -0400

    setup storybook config in webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -6,5 +7,17 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
-}
+  ],
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async config => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    config.module.rules[0].use[0].options.plugins.push(
+      require.resolve("babel-plugin-remove-graphql-queries")
+    )
+    return config
+  },
+} 
---
+++ src/components/nav/Nav.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Nav} from './Nav'
+
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: 'Nav',
+  component: Nav,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => <Nav {...args} />;
+
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+  /*👇 The args you need here will depend on your component */
+};
