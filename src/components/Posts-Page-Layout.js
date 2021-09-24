4954b0b1b552b2c2e230e9a21338d3b054a902c8
samuelaparker <samuel.a.parker00@gmail.com>
2021-09-24 15:54:47 -0400

    change to Posts-Page-Layout to attempt build fix

--- src/components/Posts-Page-Layout.js
+++ src/components/Posts-Page-Layout.js
@@ -1,41 +1,16 @@
 
 //THIS FILE IS THE TEMPLATE FOR MDX FILES (BLOG POSTS)
 import React from "react"
-import { graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx" 
 import Layout from "../components/layout"
 import components from "./mdxComponents"
 import styled from "styled-components"
 
-export default function PageTemplate({ data: { mdx } }) {
-   
-  return (
-    <>
-    <Layout>
-      <div style={{ display: "flex", flexDirection: "column", maxWidth: "750px", margin: "auto"}}>
-        <div style={{display: "flex", flexDirection: "column", margin: "auto"}}>
-        <h1 >{mdx.frontmatter.title}</h1>
-        <h4
-          style={{
-            color: "gray",
-            fontWeight: "normal",
-          }}
-        >{`${mdx.frontmatter.date} by ${mdx.frontmatter.author}`}</h4>
-        </div>
-        <MDXProvider components={components}>
-          <MDXRenderer>{mdx.body}</MDXRenderer>
-        </MDXProvider>
-      </div>
-        <Spacer />
-    </Layout>
-    </>
-  )
-  
-}
+export default function PageTemplate() {
 
-//When your site gets built, Gatsby will run your page query and pass the resulting data into your page component as a prop called data.
-export const pageQuery = graphql`
+  const data = useStaticQuery(graphql`
   query BlogPostQuery($id: String) {
     mdx(id: { eq: $id }) {
       id
@@ -49,7 +24,50 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`)
+console.log(data)
+   
+  return (
+    <>
+    <Layout>
+      <div style={{ display: "flex", flexDirection: "column", maxWidth: "750px", margin: "auto"}}>
+        <div style={{display: "flex", flexDirection: "column", margin: "auto"}}>
+        <h1 >{data.mdx.frontmatter.title}</h1>
+        <h4
+          style={{
+            color: "gray",
+            fontWeight: "normal",
+          }}
+        >{`${data.mdx.frontmatter.date} by ${data.mdx.frontmatter.author}`}</h4>
+        </div>
+        <MDXProvider components={components}>
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </div>
+        <Spacer />
+    </Layout>
+    </>
+  )
+  
+}
+
+//When your site gets built, Gatsby will run your page query and pass the resulting data into your page component as a prop called data.
+// export const pageQuery = graphql`
+//   query BlogPostQuery($id: String) {
+//     mdx(id: { eq: $id }) {
+//       id
+//       body
+//       frontmatter {
+//         title
+//         date
+//         author
+//         slug
+//         preview
+//       }
+//     }
+//   }
+// `
+ 
 
 const Spacer = styled.div`
    height: 9rem;
