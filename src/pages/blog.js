45cddb623a452b8ab208dbe3889f151d6915dd05
samuelaparker <samuel.a.parker00@gmail.com>
2021-09-28 14:37:32 -0400

    re-build BlogIndex

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,65 +1,94 @@
 //  THIS PAGE LISTS ALL THE BLOGS
 
-import React from "react"
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
-import styled from "styled-components"
-
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import styled from "styled-components";
 
 const BlogIndex = ({ data }) => {
-  const { edges: posts } = data.allMdx
-  console.log(data)
+
+  console.log(data.allMdx.nodes[0].frontmatter.slug);
   return (
     <Layout pageTitle="Blog">
-    <BlogListWrapper>
-      <ul>
-        {posts.map(({ node: post }) => (
-          <li key={post.id}>
-            <BlogLink to={`${post.frontmatter.slug}`}>
-              <h2>{post.frontmatter.title}</h2>
+      <BlogListWrapper>
+        {data.allMdx.nodes.map(node => (
+          <article key={node.frontmatter.id}>
+            <BlogLink to={`${node.frontmatter.slug}`}>
+              <h2>{node.frontmatter.title}</h2>
               <h4 style={{ lineHeight: "1.7", fontWeight: "normal" }}>
-                {post.frontmatter.preview}
+                {node.frontmatter.preview}
               </h4>
               <h4>Read more →</h4>
             </BlogLink>
-          </li>
+          </article>
         ))}
-      </ul>
-    </BlogListWrapper>
+      </BlogListWrapper>
     </Layout>
-  )
-}
+  );
+};
+// const BlogIndex = ({ data }) => {
+//   return (
+//     <Layout pageTitle="My Blog Posts">
+//       <BlogListWrapper>
+//       {
+//         data.allMdx.nodes.map((node) => (
+//           <article key={node.id}>
+//             <BlogLink>{node.frontmatter.title}</BlogLink>
+//             <p>Posted: {node.frontmatter.date}</p>
+//           </article>
+//         ))
+//       }
+//       </BlogListWrapper>
+//     </Layout>
+//   )
+// }
 
-export const pageQuery = graphql`
-  query blogIndex {
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          body
-          frontmatter {
-            title
-            preview
-            slug
-          }
+// export const pageQuery = graphql`
+//   query blogIndex {
+//     allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+//       edges {
+//         node {
+//           id
+//           body
+//           frontmatter {
+//             title
+//             preview
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          preview
+          slug
         }
+        id
+        body
       }
     }
   }
-`
+`;
 
-export default BlogIndex
-
+export default BlogIndex;
 
 const BlogListWrapper = styled.div`
-    max-width: 750px;
-    padding-top: 2em;
-    margin: auto;
-    
+  max-width: 750px;
+  height: 100vh;
+  padding-top: 2em;
+  margin: auto;
 `;
-const BlogLink = styled(props => <Link {...props} />)`
-    color: black;
-    &:hover {
+const BlogLink = styled((props) => <Link {...props} />)`
+  color: black;
+  &:hover {
     color: rgb(92, 92, 92);
-    }
+  }
 `;
