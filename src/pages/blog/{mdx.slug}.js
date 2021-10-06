362cc23ba1a667f415d205f00e2361d8c208f152
samuelaparker <samuel.a.parker00@gmail.com>
2021-10-05 20:23:02 -0400

    add hero_image to blog with GatsbyImage and getImage

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,17 +2,21 @@ import React from "react";
 import Layout from "../../components/layout";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 //blog posts are created with the gatsby File System Route API
 
 const BlogPost = ({ data }) => {
-  
   let { title } = data.mdx.frontmatter;
   let { body } = data.mdx;
-  console.log(title);
+
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
+  console.log(data.mdx.frontmatter.hero_image);
 
   return (
     <Layout pageTitle={title}>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
   );
@@ -29,6 +33,12 @@ export const query = graphql`
         slug
         preview
         title
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
